d721a7bc68e7c555e9bb7acb4d57c1bace609558
amiecollins <a_collins84448@fanshaweonline.ca>
2019-03-10 21:50:30 -0400

    final commit

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@
 
 	console.log("Javascript is linked up");
 
-	var theButtons = document.querySelectorAll('.pastEventsTitle');
+	var theButtons = document.querySelectorAll('.Title');
 
 	function toggleContent() {
 		// Toggle Past Event Content
 		console.log(this.id);
-		document.getElementById(this.id+"Content").classList.toggle("visible");	
+		document.getElementById("katContent").classList.remove("visible");
+		document.getElementById("amieContent").classList.remove("visible");
+		document.getElementById(this.id+"Content").classList.add("visible");	
 	}
 
 	theButtons.forEach(currentButton => currentButton.addEventListener('click', toggleContent));
